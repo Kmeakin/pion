e111022891afa786a786e0df82022d6f09b9f931
Karl Meakin <karlwfmeakin@gmail.com>
2024-09-06 23:54:53 +0100

    rename `pion-surface` to `pion-lexer`

--- fuzz/src/lexer.rs
+++ fuzz/src/lexer.rs
@@ -3,6 +3,6 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|text: &str| {
-    let tokens = pion_surface::lexer::lex(text);
+    let tokens = pion_lexer::lex(text);
     std::hint::black_box(tokens.count());
 });
