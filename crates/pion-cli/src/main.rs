a7491fd72d4bc8cbea8f64622b5204b8096a48d5
Karl Meakin <karlwfmeakin@gmail.com>
2024-09-19 23:14:46 +0100

    elab in `main`

--- crates/pion-cli/src/main.rs
+++ crates/pion-cli/src/main.rs
@@ -3,12 +3,14 @@ use std::str::FromStr;
 
 use camino::Utf8PathBuf;
 use clap::Parser;
+use codespan_reporting::diagnostic;
 use codespan_reporting::files::Files;
+use codespan_reporting::term::emit;
 
 #[derive(Debug, Parser)]
 #[command(version)]
 pub enum Command {
-    Check { paths: Vec<PathOrStdin> },
+    Check { path: PathOrStdin },
 }
 
 #[derive(Clone, Debug)]
@@ -61,43 +63,52 @@ impl FromStr for PathOrStdin {
     }
 }
 
-fn check(paths: &[PathOrStdin]) {
-    let mut diagnostics = Vec::new();
-    let mut writer = codespan_reporting::term::termcolor::StandardStream::stderr(
+fn check(path: &PathOrStdin) {
+    let mut stderr = codespan_reporting::term::termcolor::StandardStream::stderr(
         codespan_reporting::term::termcolor::ColorChoice::Auto,
     );
     let mut files = codespan_reporting::files::SimpleFiles::new();
-    let mut file_ids = Vec::new();
     let config = codespan_reporting::term::Config::default();
 
-    for path in paths {
-        match path.read() {
-            Ok(text) => {
-                let file_id = files.add(path.name(), text);
-                file_ids.push(file_id);
-            }
-            Err(err) => eprintln!("Error reading {}: {}", path.name(), err),
+    let file_id = match path.read() {
+        Ok(text) => files.add(path.name(), text),
+        Err(err) => {
+            eprintln!("Error reading {}: {}", path.name(), err);
+            return;
         }
-    }
+    };
+
+    let mut emit = |diagnostic| {
+        codespan_reporting::term::emit(&mut stderr, &config, &files, &diagnostic)
+            .expect("Unable to emit diagnostic");
+    };
 
     let bump = bumpalo::Bump::new();
-
-    for file in file_ids {
-        let text = files.source(file).unwrap();
-        let tokens = pion_lexer::lex(text);
-        let (_expr, diags) = pion_parser::parse_expr(file, tokens, &bump);
-        diagnostics.extend(diags);
-    }
+    let text = files.source(file_id).unwrap();
+    let tokens = pion_lexer::lex(text);
+    let (expr, diagnostics) = pion_parser::parse_expr(file_id, tokens, &bump);
 
     for diagnostic in diagnostics {
-        codespan_reporting::term::emit(&mut writer, &config, &files, &diagnostic)
-            .expect("Unable to emit diagnostic");
+        emit(diagnostic);
     }
+
+    let mut interner = pion_interner::Interner::default();
+    let mut elab = pion_elab::Elaborator::new(&bump, &mut interner, file_id);
+    let (expr, r#type) = elab.synth_expr(expr.as_ref());
+    let diagnostics = elab.finish();
+
+    for diagnostic in diagnostics {
+        emit(diagnostic);
+    }
+
+    let mut out = String::new();
+    pion_core::print::type_ann_expr(&mut out, &expr, &r#type).unwrap();
+    println!("{out}");
 }
 
 fn main() {
     let cmd = Command::parse();
     match cmd {
-        Command::Check { paths } => check(&paths),
+        Command::Check { path } => check(&path),
     }
 }
